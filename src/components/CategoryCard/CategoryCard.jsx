0b286c20194e05439e33bf8bb14691e3efd8999d
Artem Glebov <artem.glebov2004@yandex.ru>
2024-01-22 12:35:20 +0300

    доделал загрузку в CategoryCard

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -9,104 +9,88 @@ import ButtonIcon from "../ButtonIcon/ButtonIcon";
 
 const CategoryCard = () => {
   const { id } = useParams();
-  const [recipeDetails, setRecipeDetails] = useState(null);
-  const [popular, setPopular] = useState([]);
+  const [recipe, setRecipe] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const getRecipe = () => {
-    const check = localStorage.getItem("product");
-
-    if (check) {
-      const parsedRecipes = JSON.parse(check);
-      // Найдем рецепт по id
-      const selectedRecipe = parsedRecipes.find((recipe) => recipe.id === +id);
-
-      if (selectedRecipe) {
-        setPopular([selectedRecipe]); // Показываем только выбранный рецепт
-      } else {
-        console.log("Recipe not found");
-      }
-    } else {
-      console.log("error");
-    }
+  const fetchRecipeDetails = async (recipeId) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API}`
+    );
+    const recipe = await data.json();
+    setRecipe(recipe);
+    console.log(recipe);
   };
 
   useEffect(() => {
-    getRecipe();
+    setTimeout(() => {
+      setIsLoading(true);
+    }, 2000);
+    fetchRecipeDetails(id);
   }, [id]);
 
-  // const fetchRecipeDetails = async (recipeId) => {
-  //   const data = await fetch(
-  //     `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API}`
-  //   );
-  //   const recipe = await data.json();
-  //   setRecipeDetails(recipe);
-  //   console.log(recipe);
-  // };
-
-  // useEffect(() => {
-  //   fetchRecipeDetails(id);
-  // }, [id]);
-
-  return (
+  return isLoading ? (
     <div className={styles["category-card"]}>
       <div className={styles["category-card-navigate"]}>
         <Link to="/">Home</Link>
         <RiArrowRightSLine size={20} />
         <Link to="/category">Category</Link>
         <RiArrowRightSLine size={20} />
-        <p>БЛЮДО:</p>
+        <p>{id}</p>
       </div>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id} className={styles["category-card-info"]}>
-            <div className={styles["category-card-info-header"]}>
-              <h2>{recipe.title}</h2>
-              <div className={styles["category-card-info-header-social"]}>
-                <ButtonIcon>
-                  <IoHeart size={30} color="red" />
-                </ButtonIcon>
-                <ButtonIcon>
-                  <IoShareSocial size={30} color="#1cb96d" />
-                </ButtonIcon>
-              </div>
-            </div>
-            <hr color="#D0DBEA" />
-            <div className={styles["category-card-product"]}>
-              <img src={recipe.image} alt="" />
-              <ul className={styles["category-card-ingredients"]}>
-                <h2>Ingredients:</h2> <br />
-                {recipe.extendedIngredients.map((ingredient) => (
-                  <li key={ingredient.key}>{ingredient.original}</li>
-                ))}
-              </ul>
-            </div>
-            <div className={styles["category-card-time"]}>
-              <ul className={styles['category-card-time-list']}>
-                  <li>
-                    <h4>Time:</h4>
-                    <p>{recipe.readyInMinutes} min</p>
-                  </li> <hr />
-                  <li>
-                    <h4>Servings:</h4>
-                    <p>{recipe.servings}</p>
-                  </li> <hr />
-                  <li>
-                    <h4>Price:</h4>
-                    <p>{recipe.pricePerServing} ₽</p>
-                  </li>
-              </ul>
-            </div>
-            <div className={styles['category-card-instructions']}>
-                  <h2>Instructions:</h2>
-                  <ol>
-                    <li dangerouslySetInnerHTML={{ __html: recipe.instructions}}></li>
-                  </ol>
-            </div>
+      {recipe && (
+        <div key={recipe.id} className={styles["category-card-info"]}>
+        <div className={styles["category-card-info-header"]}>
+          <h2>{recipe.title}</h2>
+          <div className={styles["category-card-info-header-social"]}>
+            <ButtonIcon>
+              <IoHeart size={30} color="red" />
+            </ButtonIcon>
+            <ButtonIcon>
+              <IoShareSocial size={30} color="#1cb96d" />
+            </ButtonIcon>
           </div>
+        </div>
+        <hr color="#D0DBEA" />
+        <div className={styles["category-card-product"]}>
+          <img src={recipe.image} alt="" />
+          <ul className={styles["category-card-ingredients"]}>
+            <h2>Ingredients:</h2> <br />
+            {recipe.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.key}>{ingredient.original}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles["category-card-time"]}>
+          <ul className={styles['category-card-time-list']}>
+              <li>
+                <h4>Time:</h4>
+                <p>{recipe.readyInMinutes} min</p>
+              </li> <hr />
+              <li>
+                <h4>Servings:</h4>
+                <p>{recipe.servings}</p>
+              </li> <hr />
+              <li>
+                <h4>Price:</h4>
+                <p>{recipe.pricePerServing} ₽</p>
+              </li>
+          </ul>
+        </div>
+        <div className={styles['category-card-instructions']}>
+              <h2>Instructions:</h2>
+              <ol>
+                <li dangerouslySetInnerHTML={{ __html: recipe.instructions}}></li>
+              </ol>
+        </div>
+      </div>
+      )}
+      {/* {recipeDetails.map((recipe) => {
+        return (
+          
         );
-      })}
+      })} */}
     </div>
-  );
+  ) : <Loading />
 };
 
 export default CategoryCard;
