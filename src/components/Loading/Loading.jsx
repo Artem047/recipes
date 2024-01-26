ec1f1170d9e841188e652b1595fffd0b6b551184
Artem Glebov <artem.glebov2004@yandex.ru>
2024-01-26 16:56:18 +0300

    поправка

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,11 +2,7 @@ import styles from './Loading.module.css';
 
 const Loading = () => {
   return (
-<<<<<<< HEAD
-    <div className={styles['loading']}>Loading...</div>
-=======
     <div className={styles['load']}>Loading...</div>
->>>>>>> refs/remotes/origin/main
   )
 }
 
